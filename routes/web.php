3149960619afd05ccab2917fa5a77c89fbe66a0b
PasUnNom <148554594+PasUnNom@users.noreply.github.com>
2024-03-15 15:39:45 +0100

    setup

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
