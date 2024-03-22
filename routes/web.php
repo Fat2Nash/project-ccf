7a6aaf15a256d8986cef4a76b7da7bfef830f8c8
PasUnNom <148554594+PasUnNom@users.noreply.github.com>
2024-03-22 13:29:27 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/clients', function () {
+    return view('client');
+})->middleware(['auth', 'verified'])->name('client');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
