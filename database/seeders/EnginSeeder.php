266c86f898674289fa9c720065f1bb4a73db7c24
Sh1muru <121982875+Sh1muru@users.noreply.github.com>
2024-03-22 11:58:37 +0100

    mmmmmh

---
+++ database/seeders/EnginSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EnginSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+                // Créez un utilisateur de test
+                DB::table('engins')->insert([
+                    'categorie' => 'Pelle',
+                    'marque' => 'Caterpillar',
+                    'modele' => '320',
+                    'description' => 'Pelle hydraulique sur chenilles',
+                    'compteur_heures' => 1500,
+                    'statut' => 'Disponible',
+                    'maintenance' => 0,
+                    'cree_le' => now(),
+                    'mis_a_jours_le' => now(),
+                ]);
+                // Créez un utilisateur de test
+                DB::table('engins')->insert([
+                    'categorie' => 'Mini Pelle',
+                    'marque' => 'Caterpillar',
+                    'modele' => '123',
+                    'description' => 'Mini pelle hydraulique sur chenilles',
+                    'compteur_heures' => 500,
+                    'statut' => 'Louer',
+                    'maintenance' => 0,
+                    'cree_le' => now(),
+                    'mis_a_jours_le' => now(),
+                ]);
+    }
+}
