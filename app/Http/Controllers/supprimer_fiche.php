c297b969a3aa2cfd55ae90b29c135d02282829a5
PasUnNom <erwan.m@wan-host.fr>
2024-06-14 00:49:16 +0200

    Update supprimer_fiche.php

--- app/Http/Controllers/supprimer_fiche.php
+++ app/Http/Controllers/supprimer_fiche.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use App\Models\Engin;
+use App\Models\Location;
 
 
 class supprimer_fiche extends Controller
@@ -18,6 +19,12 @@ class supprimer_fiche extends Controller
     { {
             Engin::where('id_engins', $id)->delete();
             return redirect('/engins');
+        }
+    }
+    public function supprimerloc($id)
+    { {
+            Location::where('id_loc_engin', $id)->delete();
+            return redirect('/locations');
         }
     }
 }
